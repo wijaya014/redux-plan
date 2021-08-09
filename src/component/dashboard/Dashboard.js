afdbe4e3eae29609ff1724a7a42af7dfe0433ab9
wijaya014 <wijaya.agus014@gmail.com>
2021-08-09 17:48:08 +0700

    error sync dasboard cmponent to firebase

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Notification from "./Notification";
 import ProjectList from "../project/ProjectList";
 import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class Dashboard extends Component {
   render() {
@@ -23,9 +25,17 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log("this obj ", state);
   return {
-    projects: state.project.projects,
+    projects: state.firestore.ordered.projects,
   };
 };
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    {
+      collection: "pojects",
+    },
+  ])
+)(Dashboard);
